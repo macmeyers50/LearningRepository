a8b54c5aca17e016c68d12dfe8b5810e88463711
macmeyers50 <macmeyers50@gmail.com>
2020-01-25 17:27:59 -0500

    FirstCommit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          You should die.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=qP5Sey7mVPk"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Joe sucks more eggs than a yolk remover xD
         </a>
       </header>
     </div>
