da5a448d18e7c47ddb7b78ef8283595564461029
macmeyers50 <macmeyers50@gmail.com>
2020-01-26 12:39:41 -0500

    change to app

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import DangerButton from './Components/DangerButton.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <DangerButton/>
         <p>
           You should die.
         </p>
