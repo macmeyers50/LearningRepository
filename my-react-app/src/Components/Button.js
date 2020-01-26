b91b38964e579990538be0939727d02f33d64702
macmeyers50 <macmeyers50@gmail.com>
2020-01-26 12:46:01 -0500

    change to app

--- my-react-app/src/Components/Button.js
+++ my-react-app/src/Components/Button.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 class Button extends Component {
   render() {
-    // ...
+    return(
+        <button type="button">Click Me!</button>
+    )
   }
 }
 export default Button;
